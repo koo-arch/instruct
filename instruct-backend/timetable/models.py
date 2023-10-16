fcefc6fc15ea7f0e822644cf28f100b225765bf3
koo-arch <yukshw.ko.0623@gmail.com>
2023-10-16 11:41:09 +0900

    update db

--- instruct-backend/timetable/models.py
+++ instruct-backend/timetable/models.py
@@ -5,4 +5,3 @@ class TimeTable(models.Model):
     school_period = models.IntegerField()
     start_time = models.TimeField()
     end_time = models.TimeField()
-    is_current_period = models.BooleanField(default=False)
